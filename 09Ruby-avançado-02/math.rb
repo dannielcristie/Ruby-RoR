b120e3c091a759df1f9c914464df9000da482748
Danniel Cristie <dannielcristie@gmail.com>
2022-05-22 17:32:00 -0300

    Aula: Math,  introdução ao modulo Math do ruby.

---
+++ 09Ruby-avançado-02/math.rb
@@ -0,0 +1,10 @@
+# Math, modulo nativo do ruby para realizar operações matematica
+
+puts Math.sqrt(64) # raiz quadrada
+puts Math.log10(1000) # logaritmo na base 10
+
+radian = 30 * (Math::PI / 180) # graus para radianos
+puts Math.cos(radian) # cosseno de 30
+
+puts Math::PI # valor de Pi (dois pois é um module)
+puts Math::E # valor de e
