1b24901cba01395e3feed16f049494c08369ad29
Danniel Cristie <dany14012@gmail.com>
2022-05-30 09:21:50 -0300

    Aula(Rails): criando rotas do CRUD com o metodo Recourses.

--- 12Controller-rails/blog-example/config/routes.rb
+++ 12Controller-rails/blog-example/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'site', to: 'site#index'
   #mudando o end point para o barra(root)
   root to: 'site#index'
-  #Crianção de rotas de um CRUD
+  #Crianção de rotas de um CRUD, para acessar use .../rails/info/routes
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
