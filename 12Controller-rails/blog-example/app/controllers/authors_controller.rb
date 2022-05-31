3f67d61c9e5c47b84e3009d414da84a23a0a2252
Danniel Cristie <dany14012@gmail.com>
2022-05-31 10:23:41 -0300

    Aula(Rails): callbacks dos controller.

--- 12Controller-rails/blog-example/app/controllers/authors_controller.rb
+++ 12Controller-rails/blog-example/app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: %i[ show edit update destroy ]
+  
 
   # GET /authors or /authors.json
   def index
--- 12Controller-rails/blog-example/config/routes.rb
+++ 12Controller-rails/blog-example/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :authors
   #mudando o end point da rota
   get 'site', to: 'site#index'
   #mudando o end point para o barra(root)
   root to: 'site#index'
   #Crianção de rotas de um CRUD, para acessar use .../rails/info/routes
-  resources :users
+  #resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
