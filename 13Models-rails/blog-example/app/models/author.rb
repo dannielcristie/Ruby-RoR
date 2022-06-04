c5b1b4ee83bd1397a0f1ca426c9f225f91e32684
Danniel Cristie <dany14012@gmail.com>
2022-06-03 22:22:08 -0300

    Aula(Rails): validação de dados de entradas com helper validates do Actice Record.

--- 13Models-rails/blog-example/app/models/author.rb
+++ 13Models-rails/blog-example/app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    #validando a entradas de dados ao banco de dados
+    validates :First_name, presence: true, length: {minimum:2, maximum: 100}
+    validates :last_name, presence: true
+    validates :description, allow_nil: true, length: {maximum: 500}
 end
