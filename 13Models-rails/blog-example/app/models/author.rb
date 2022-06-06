5613e6d0d9f8d125e7903c13f942bf629abc922b
Danniel Cristie <dany14012@gmail.com>
2022-06-06 16:17:44 -0300

    Aula(Rails): introdução a Callback dos models.

--- 13Models-rails/blog-example/app/models/author.rb
+++ 13Models-rails/blog-example/app/models/author.rb
@@ -3,4 +3,13 @@ class Author < ApplicationRecord
     validates :First_name, presence: true, length: {minimum:2, maximum: 100}
     validates :last_name, presence: true
     validates :description, allow_nil: true, length: {maximum: 500}
+
+    #callback do models
+    after_validation :titleize_last_name, if: Proc.new { |a| a.last_name.present?}, on: :create
+
+    private
+    #metodo para titleize o last name depois de validar
+    def titleize_last_name
+        self.last_name = last_name.titleize
+    end
 end
