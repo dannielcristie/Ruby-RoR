d1611b3ea01dae67cc9a2ecc80afdc3532635e86
Danniel Cristie <dany14012@gmail.com>
2022-06-12 14:33:23 -0300

    Aula(Rails) : criando a views do chat.

--- 17Action-cable-rails/chat/app/javascript/channels/chat_channel.js
+++ 17Action-cable-rails/chat/app/javascript/channels/chat_channel.js
@@ -2,6 +2,7 @@ import consumer from "./consumer"
 
 consumer.subscriptions.create("ChatChannel", {
   connected() {
+    console.log('conectado ao chat room!');
     // Called when the subscription is ready for use on the server
   },
 
