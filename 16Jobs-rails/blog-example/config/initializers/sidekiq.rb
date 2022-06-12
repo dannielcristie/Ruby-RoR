fcfd65949c1030c52ad8bb5d029d2fca7dce084b
Danniel Cristie <dany14012@gmail.com>
2022-06-12 00:01:53 -0300

    Exercicio do modulo: exercicios sobre jobs, sidekiq, sidekiq cron.

--- 16Jobs-rails/blog-example/config/initializers/sidekiq.rb
+++ 16Jobs-rails/blog-example/config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 jobs = {
     'new_post_notification_job' =>{
-        'cron' => '* * * * *',
+        'cron' => '0 10 * * 6',
         'class' => 'NewPostsNotificationJob',
         'queue' => 'notification',
         'active_job' => true
