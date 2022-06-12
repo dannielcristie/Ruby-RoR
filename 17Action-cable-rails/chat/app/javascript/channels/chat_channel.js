bbab0a82a00e0e23e911ecee054a7adc136dca27
Danniel Cristie <dany14012@gmail.com>
2022-06-12 16:58:06 -0300

    Aula(Rails) : enviando messagens em Realtime com action cable.

--- 17Action-cable-rails/chat/app/javascript/channels/chat_channel.js
+++ 17Action-cable-rails/chat/app/javascript/channels/chat_channel.js
@@ -1,8 +1,8 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("ChatChannel", {
+const chatChannel = consumer.subscriptions.create("ChatChannel", {
   connected() {
-    console.log('conectado ao chat room!');
+    console.log('conectado ao chat room!')
     // Called when the subscription is ready for use on the server
   },
 
@@ -12,9 +12,22 @@ consumer.subscriptions.create("ChatChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
+    $('#messages').append(`<p>${data.message}</p>`)
+    $('html, body').animate({ scrollTop: $(document).height() }, 0)
   },
 
-  speak: function() {
-    return this.perform('speak');
+  speak: function (message) {
+    return this.perform('speak', { message })
   }
-});
+})
+
+$(document).on('turbolinks:load', function () {
+  $("#message_form").on('submit', function(e){
+    e.preventDefault();
+    let message = $('#message_to_sent').val();
+    if (message.length > 0) {
+      chatChannel.speak(message);
+      $('#message_to_sent').val('')
+    }
+  });
+})
